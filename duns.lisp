e7905d70a758a7e4a3d8f23aad838f109d87b391
Emil Segerbäck <emilsegerback@gmail.com>
2020-01-16 17:42:19 +0100

    Add unit testing framework

--- duns.lisp
+++ duns.lisp
@@ -1,3 +1,36 @@
+;; Testing framework (from practical common lisp)
+(defmacro with-gensyms ((&rest names) &body body)
+  `(let ,(loop for n in names collect `(,n (gensym)))
+     ,@body))
+
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  "Define a test function. Within a test function we can call
+   other test functions or use 'check' to run individual test
+   cases."
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+
+(defmacro check (&body forms)
+  "Run each expression in 'forms' as a test case."
+  `(combine-results
+    ,@(loop for f in forms collect `(report-result ,f ',f))))
+
+(defmacro combine-results (&body forms)
+  "Combine the results (as booleans) of evaluating 'forms' in order."
+  (with-gensyms (result)
+    `(let ((,result t))
+       ,@(loop for f in forms collect `(unless ,f (setf ,result nil)))
+       ,result)))
+
+(defun report-result (result form)
+  "Report the results of a single test case. Called by 'check'."
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *test-name* form)
+  result)
+
+;; Vector math
 (defun invert-vec (v)
   (destructuring-bind (x y z) v
     (list (- x) (- y) (- z))))
@@ -48,6 +81,10 @@
       (list (- (* y1 z2) (* z1 y2))
             (- (* z1 x2) (* x1 z2))
             (- (* x1 y2) (* y1 x2))))))
+
+;; (deftest test-invert-vec ()
+;;   (check
+;;     (= )))
 
 (defclass physical-particle ()
   ((particle-position
